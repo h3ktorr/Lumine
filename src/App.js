b319a7804ccdd45b68372e2f1a17845ad6820955
Kelvin Achi <kellywizy09@gmail.com>
2024-01-19 08:56:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import mensTop from "./Components/Assets/mens_top";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {mensTop.map((top) => {
+        const { id, name, image, price, category } = top;
+        return (
+          <div key={id}>
+            <h1>{name}</h1>
+            <img src={image[1]} />
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Components/Assets/mens_top.js
@@ -0,0 +1,560 @@
+import ginza2 from "./men tops/KHM031529-413-BACK_720x.jpg";
+import ginza1 from "./men tops/KHM031529-413-FRONT_720x.jpg";
+import ginza3 from "./men tops/KHM031529-413-DETAIL-1_720x.jpg";
+import ginza4 from "./men tops/KHM031529-413-DETAIL-2_720x.jpg";
+import ginza5 from "./men tops/KHM031529-413-DETAIL-3_720x.jpg";
+import Anorak1 from "./men tops/KHM010418-116-FRONT_720x.jpg";
+import Anorak2 from "./men tops/KHM010418-116-BACK_720x.jpg";
+import Anorak3 from "./men tops/KHM010418-116-DETAIL-3_720x.jpg";
+import Anorak4 from "./men tops/KHM010418-116-DETAIL-1_720x.jpg";
+import Anorak5 from "./men tops/KHM010418-116-DETAIL-2_720x.jpg";
+import Crewneck1 from "./men tops/KHM031694-001-FRONT_720x.jpg";
+import Crewneck2 from "./men tops/KHM031694-001-BACK_720x.jpg";
+import Crewneck3 from "./men tops/KHM031694-001-DETAIL-1_720x.jpg";
+import Crewneck4 from "./men tops/KHM031694-001-DETAIL-2_720x.jpg";
+import Nutcracker1 from "./men tops/KHM031692-413-FRONT_720x.jpg";
+import Nutcracker2 from "./men tops/KHM031692-413-BACK_720x.jpg";
+import Nutcracker3 from "./men tops/KHM031692-413-DETAIL-1_720x.jpg";
+import Nutcracker4 from "./men tops/KHM031692-413-DETAIL-2_720x.jpg";
+import Wreath1 from "./men tops/KHM031688-212-FRONT_720x.jpg";
+import Wreath2 from "./men tops/KHM031688-212-BACK_720x.jpg";
+import Wreath3 from "./men tops/KHM031688-212-DETAIL-1_720x.jpg";
+import Wreath4 from "./men tops/KHM031688-212-DETAIL-2_720x.jpg";
+import Orbit1 from "./men tops/KHM031689-302-FRONT_720x.jpg";
+import Orbit2 from "./men tops/KHM031689-302-BACK_720x.jpg";
+import Orbit3 from "./men tops/KHM031689-302-DETAIL-1_720x.jpg";
+import Orbit4 from "./men tops/KHM031689-302-DETAIL-2_720x.jpg";
+import Boxy1 from "./men tops/KHM031764-413-FRONT_720x.jpg";
+import Boxy2 from "./men tops/KHM031764-413-BACK_720x.jpg";
+import Boxy3 from "./men tops/KHM031764-413-DETAIL-1_720x.jpg";
+import Boxy4 from "./men tops/KHM031764-413-DETAIL-2_720x.jpg";
+import Boxy5 from "./men tops/KHM031764-413-DETAIL-3_720x.jpg";
+import Wale1 from "./men tops/KHM031663-441-FRONT_720x.jpg";
+import Wale2 from "./men tops/KHM031663-441-BACK_720x.jpg";
+import Wale3 from "./men tops/KHM031663-441-DETAIL-1_720x.jpg";
+import Wale4 from "./men tops/KHM031663-441-DETAIL-2_720x.jpg";
+import Wale5 from "./men tops/KHM031663-441-DETAIL-3_720x.jpg";
+import Wyona1 from "./men tops/KHM031665-104-FRONT_720x.jpg";
+import Wyona2 from "./men tops/KHM031665-104-BACK_720x.jpg";
+import Wyona3 from "./men tops/SHOT7_0370_806016e2-3a5a-4ca7-9b75-9f8ab58844b9_720x.jpg";
+import Wyona4 from "./men tops/SHOT8_0385_bf70bfe9-8b44-4b9f-8bbe-f50b1bc85175_720x.jpg";
+import Wyona5 from "./men tops/KHM031665-104-DETAIL-1_720x.jpg";
+import Langdon1 from "./men tops/KHM031660-127-FRONT_720x.jpg";
+import Langdon2 from "./men tops/KHM031660-127-BACK_720x.jpg";
+import Langdon3 from "./men tops/SHOT30_1840_720x.jpg";
+import Langdon4 from "./men tops/KHM031660-127-DETAIL-1_720x.jpg";
+import Langdon5 from "./men tops/KHM031660-127-DETAIL-2_720x.jpg";
+import Fleece1 from "./men tops/KHM031415-002-FRONT_720x.jpg";
+import Fleece2 from "./men tops/KHM031415-002-BACK_720x.jpg";
+import Fleece3 from "./men tops/KHM031415-002-DETAIL-1_720x.jpg";
+import Fleece4 from "./men tops/KHM031415-002-DETAIL-2_720x.jpg";
+import Fleece5 from "./men tops/KHM031415-002-DETAIL-3_720x.jpg";
+import Sandrift1 from "./men tops/KHM031415-104-FRONT_720x.jpg";
+import Sandrift2 from "./men tops/KHM031415-104-BACK_720x.jpg";
+import Sandrift3 from "./men tops/SHOT65_3754_720x.jpg";
+import Sandrift4 from "./men tops/KHM031415-104-DETAIL-1_720x.jpg";
+import Sandrift5 from "./men tops/KHM031415-104-DETAIL-2_720x.jpg";
+import Coleman1 from "./men tops/KHM031662-423-FRONT_720x.jpg";
+import Coleman2 from "./men tops/KHM031662-423-BACK_720x.jpg";
+import Coleman3 from "./men tops/SHOT70_3849_720x.jpg";
+import Coleman4 from "./men tops/KHM031662-423-DETAIL-1_720x.jpg";
+import Coleman5 from "./men tops/KHM031662-423-DETAIL-2_720x.jpg";
+import Strauss1 from "./men tops/KHM031524-002-FRONT_720x.jpg";
+import Strauss2 from "./men tops/KHM031524-002-BACK_720x.jpg";
+import Strauss3 from "./men tops/SHOT43_2627_720x.jpg";
+import Strauss4 from "./men tops/KHM031524-002-DETAIL-1_720x.jpg";
+import Strauss5 from "./men tops/KHM031524-002-DETAIL-2_720x.jpg";
+import Ornamental1 from "./men tops/KHM031926-413-FRONT_720x.jpg";
+import Ornamental2 from "./men tops/KHM031926-413-BACK_720x.jpg";
+import Ornamental3 from "./men tops/KHM031926-413-DETAIL-1_720x.jpg";
+import Ornamental4 from "./men tops/KHM031926-413-DETAIL-2_720x.jpg";
+import Knicks1 from "./men tops/KHM031616-101-FRONT_720x.jpg";
+import Knicks2 from "./men tops/KHM031616-101-BACK_720x.jpg";
+import Knicks5 from "./men tops/KHM031616-101-DETAIL-1_720x.jpg";
+import Knicks4 from "./men tops/KHM031616-101-DETAIL-2_720x.jpg";
+import Knicks3 from "./men tops/KHM031616-101-DETAIL-3_720x.jpg";
+import Carson1 from "./men tops/KHM031614-101-FRONT_720x.jpg";
+import Carson2 from "./men tops/KHM031614-101-BACK_720x.jpg";
+import Carson3 from "./men tops/KHM031614-101-DETAIL-1_720x.jpg";
+import Carson4 from "./men tops/KHM031614-101-DETAIL-2_720x.jpg";
+import Carson5 from "./men tops/KHM031614-101-DETAIL-3_720x.jpg";
+import Woodpoint1 from "./men tops/KHM031618-413-FRONT_720x.jpg";
+import Woodpoint2 from "./men tops/KHM031618-413-BACK_720x.jpg";
+import Woodpoint3 from "./men tops/KHM031618-413-DETAIL-1_720x.jpg";
+import Woodpoint4 from "./men tops/KHM031618-413-DETAIL-2_720x.jpg";
+import Woodpoint5 from "./men tops/KHM031618-413-DETAIL-3_720x.jpg";
+import Retro1 from "./men tops/KHM031646-002-FRONT_720x.jpg";
+import Retro2 from "./men tops/KHM031646-002-BACK_720x.jpg";
+import Retro3 from "./men tops/KHM031646-002-DETAIL-1_720x.jpg";
+import Retro4 from "./men tops/KHM031646-002-DETAIL-2_720x.jpg";
+import Retro5 from "./men tops/KHM031646-002-DETAIL-3_720x.jpg";
+import Williams1 from "./men tops/KHM031647-413-FRONT_720x.jpg";
+import Williams2 from "./men tops/KHM031647-413-BACK_720x.jpg";
+import Williams3 from "./men tops/KHM031647-413-DETAIL-1_720x.jpg";
+import Williams4 from "./men tops/KHM031647-413-DETAIL-2_720x.jpg";
+import MilkRugby1 from "./men tops/KHM031617-116-FRONT_720x.jpg";
+import MilkRugby2 from "./men tops/KHM031617-116-BACK_720x.jpg";
+import MilkRugby3 from "./men tops/KHM031617-116-DETAIL-1_720x.jpg";
+import MilkRugby4 from "./men tops/KHM031617-116-DETAIL-2_720x.jpg";
+import MilkRugby5 from "./men tops/KHM031617-116-DETAIL-3_720x.jpg";
+import BlueRugby1 from "./men tops/KHM031617-413-FRONT_720x.jpg";
+import BlueRugby2 from "./men tops/KHM031617-413-BACK_720x.jpg";
+import BlueRugby3 from "./men tops/KHM031617-413-DETAIL-1_720x.jpg";
+import BlueRugby4 from "./men tops/KHM031617-413-DETAIL-2_720x.jpg";
+import BlueRugby5 from "./men tops/KHM031617-413-DETAIL-3_720x.jpg";
+import WhiteCourt1 from "./men tops/KHM031641-002-FRONT_720x.jpg";
+import WhiteCourt2 from "./men tops/KHM031809-002-BACK_2d63482c-1a89-42dd-b2fc-ad5322a88d71_720x.png";
+import WhiteCourt3 from "./men tops/KHM031641-002-DETAIL-1_720x.jpg";
+import WhiteCourt4 from "./men tops/KHM031641-002-DETAIL-2_720x.jpg";
+import BlackCourt1 from "./men tops/KHM031641-001-FRONT_720x.jpg";
+import BlackCourt2 from "./men tops/KHM031641-001-BACK_720x.jpg";
+import BlackCourt3 from "./men tops/KHM031641-001-DETAIL-1_720x.jpg";
+import BlackCourt4 from "./men tops/KHM031641-001-DETAIL-2_720x.jpg";
+import NyTee3 from "./men tops/KHM031638-001-DETAIL-1_720x.jpg";
+import NyTee1 from "./men tops/KHM031638-001-FRONT_720x.jpg";
+import NyTee2 from "./men tops/KHM031638-001-BACK_720x.jpg";
+import NyTee4 from "./men tops/KHM031638-001-DETAIL-2_720x.jpg";
+import NyChampion1 from "./men tops/KHM031635-104-FRONT_720x.jpg";
+import NyChampion2 from "./men tops/KHM031635-104-BACK_720x.jpg";
+import NyChampion3 from "./men tops/KHM031635-104-DETAIL-1_720x.jpg";
+import NyChampion4 from "./men tops/KHM031635-104-DETAIL-2_720x.jpg";
+import NyInsignia1 from "./men tops/KHM031640-001-FRONT_720x.jpg";
+import NyInsignia2 from "./men tops/KHM031640-001-BACK_720x.jpg";
+import NyInsignia3 from "./men tops/KHM031640-001-DETAIL-1_720x.jpg";
+import NyInsignia4 from "./men tops/KHM031640-001-DETAIL-2_720x.jpg";
+import BlueJohnson1 from "./men tops/TFSM7367LJ-FRONT_b5ff8f53-ed23-4aeb-9de0-01f0cbe9e035_720x.jpg";
+import BlueJohnson2 from "./men tops/TFSM7367LJ-BACK_720x.jpg";
+import BlueJohnson3 from "./men tops/TFSM7367LJ-DETAIL-1_720x.jpg";
+import BlueJohnson4 from "./men tops/TFSM7367LJ-DETAIL-2_720x.jpg";
+import BlueJohnson5 from "./men tops/TFSM7367LJ-DETAIL-3_720x.jpg";
+import BlueWyona1 from "./men tops/KHM031664-413-FRONT_720x.jpg";
+import BlueWyona2 from "./men tops/KHM031664-413-BACK_720x.jpg";
+import BlueWyona3 from "./men tops/KHM031664-413-DETAIL-1_720x.jpg";
+import BlueWyona4 from "./men tops/KHM031664-413-DETAIL-2_720x.jpg";
+import BlueWyona5 from "./men tops/KHM031664-413-DETAIL-3_720x.jpg";
+import GreyWyona1 from "./men tops/KHM031664-601-FRONT_720x.jpg";
+import GreyWyona2 from "./men tops/KHM031664-601-BACK_720x.jpg";
+import GreyWyona3 from "./men tops/Kith_Winter_23_LB_Pt4_LOOK11_SHOT2_2334_720x.jpg";
+import GreyWyona4 from "./men tops/KHM031664-601-DETAIL-1_720x.jpg";
+import GreyWyona5 from "./men tops/Kith_Winter_23_LB_Pt4_LOOK11_SHOT3_2384_720x.jpg";
+import WhiteDavison1 from "./men tops/KHM031547-002-FRONT_2878e858-c3f4-42bb-b9d1-3b951acd6541_720x.jpg";
+import WhiteDavison2 from "./men tops/KHM031547-002-BACK_51dd1ff5-50bb-4198-844c-dfdfa41e0e74_720x.jpg";
+import WhiteDavison3 from "./men tops/Kith_Winter_23_LB_Pt1_LOOK5_SHOT3_1844_720x.jpg";
+import WhiteDavison4 from "./men tops/KHM031547-002-DETAIL-1_720x.jpg";
+import WhiteDavison5 from "./men tops/KHM031547-002-DETAIL-2_720x.jpg";
+import GreenDavison1 from "./men tops/KHM031547-385-FRONT_1786eb3f-3817-4ebe-893f-94c3829ec49f_720x.jpg";
+import GreenDavison2 from "./men tops/KHM031547-385-BACK_4d593435-3ea3-42e8-bf0c-9b48dd51ca8a_720x.jpg";
+import GreenDavison3 from "./men tops/KHM031547-385-DETAIL-1_720x.jpg";
+import GreenDavison4 from "./men tops/KHM031547-385-DETAIL-2_720x.jpg";
+import GreenDavison5 from "./men tops/KHM031547-385-DETAIL-3_720x.jpg";
+
+
+let mensTop = [
+  {
+    id: 1,
+    name: "Wool Ginza Shirt",
+    price: 2057,
+    image: {
+      1: ginza1,
+      2: ginza2,
+      3: ginza3,
+      4: ginza4,
+      5: ginza5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 2,
+    name: "Knicks Quarter Zip Anorak",
+    price: 2259,
+    image: {
+      1: Anorak1,
+      2: Anorak2,
+      3: Anorak3,
+      4: Anorak4,
+      5: Anorak5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 3,
+    name: "Penguin Nelson Crewneck Black",
+    price: 1476,
+    image: {
+      1: Crewneck1,
+      2: Crewneck2,
+      3: Crewneck3,
+      4: Crewneck4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 4,
+    name: "Nutcracker Nelson Crewneck",
+    price: 1476,
+    image: {
+      1: Nutcracker1,
+      2: Nutcracker2,
+      3: Nutcracker3,
+      4: Nutcracker4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 5,
+    name: "Wreath Orbit Nelson Crewneck",
+    price: 1476,
+    image: {
+      1: Wreath1,
+      2: Wreath2,
+      3: Wreath3,
+      4: Wreath4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 6,
+    name: "Wreath Orbit Tee",
+    price: 1476,
+    image: {
+      1: Orbit1,
+      2: Orbit2,
+      3: Orbit3,
+      4: Orbit4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 7,
+    name: "Boxy Collared Overshirt",
+    price: 2259,
+    image: {
+      1: Boxy1,
+      2: Boxy2,
+      3: Boxy3,
+      4: Boxy4,
+      5: Boxy5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 8,
+    name: "Printed Wide Wale Cord Apollo",
+    price: 1752,
+    image: {
+      1: Wale1,
+      2: Wale2,
+      3: Wale3,
+      4: Wale4,
+      5: Wale5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 9,
+    name: "Wyona Full Zip Sweater",
+    price: 2720,
+    image: {
+      1: Wyona1,
+      2: Wyona2,
+      3: Wyona3,
+      4: Wyona4,
+      5: Wyona5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 10,
+    name: "Langdon Turtleneck",
+    price: 2997,
+    image: {
+      1: Langdon1,
+      2: Langdon2,
+      3: Langdon3,
+      4: Langdon4,
+      5: Langdon5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 11,
+    name: "Cable Fleece Quarter Zip",
+    price: 2167,
+    image: {
+      1: Fleece1,
+      2: Fleece2,
+      3: Fleece3,
+      4: Fleece4,
+      5: Fleece5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 12,
+    name: "Cable Fleece Sandrift Zip",
+    price: 2167,
+    image: {
+      1: Sandrift1,
+      2: Sandrift2,
+      3: Sandrift3,
+      4: Sandrift4,
+      5: Sandrift5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 13,
+    name: "Coleman Quarter Zip Hoodie",
+    price: 1383,
+    image: {
+      1: Coleman1,
+      2: Coleman2,
+      3: Coleman3,
+      4: Coleman4,
+      5: Coleman5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 14,
+    name: "Strauss Cross Front Cardigan",
+    price: 1429,
+    image: {
+      1: Strauss1,
+      2: Strauss2,
+      3: Strauss3,
+      4: Strauss4,
+      5: Strauss5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 15,
+    name: "Ornamental Script Nelson Hoodie",
+    price: 1522,
+    image: {
+      1: Ornamental1,
+      2: Ornamental2,
+      3: Ornamental3,
+      4: Ornamental4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 15,
+    name: "Knicks Reversible Ginza",
+    price: 2050,
+    image: {
+      1: Knicks1,
+      2: Knicks2,
+      3: Knicks3,
+      4: Knicks4,
+      5: Knicks5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 16,
+    name: "Knicks Carson Buttondown",
+    price: 1568,
+    image: {
+      1: Carson1,
+      2: Carson2,
+      3: Carson3,
+      4: Carson4,
+      5: Carson5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 17,
+    name: "Knicks Woodpoint Quarter Zip",
+    price: 1798,
+    image: {
+      1: Woodpoint1,
+      2: Woodpoint2,
+      3: Woodpoint3,
+      4: Woodpoint4,
+      5: Woodpoint5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 18,
+    name: "Knicks Retro NY Nelson Crewneck",
+    price: 1476,
+    image: {
+      1: Retro1,
+      2: Retro2,
+      3: Retro3,
+      4: Retro4,
+      5: Retro5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 19,
+    name: "Knicks Retro NY Williams III",
+    price: 1706,
+    image: {
+      1: Williams1,
+      2: Williams2,
+      3: Williams3,
+      4: Williams4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 20,
+    name: "Knicks Long Sleeve Rugby Shirt",
+    price: 1614,
+    image: {
+      1: MilkRugby1,
+      2: MilkRugby2,
+      3: MilkRugby3,
+      4: MilkRugby4,
+      5: MilkRugby5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 21,
+    name: "Knicks Long Sleeve Rugby Shirt",
+    price: 1614,
+    image: {
+      1: BlueRugby1,
+      2: BlueRugby2,
+      3: BlueRugby3,
+      4: BlueRugby4,
+      5: BlueRugby5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 22,
+    name: "Knicks Home Court Vintage Tee",
+    price: 646,
+    image: {
+      1: WhiteCourt1,
+      2: WhiteCourt2,
+      3: WhiteCourt3,
+      4: WhiteCourt4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 23,
+    name: "Knicks Home Court Vintage Tee",
+    price: 646,
+    image: {
+      1: BlackCourt1,
+      2: BlackCourt2,
+      3: BlackCourt3,
+      4: BlackCourt4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 24,
+    name: "Knicks Retro NY Vintage Tee",
+    price: 646,
+    image: {
+      1: NyTee1,
+      2: NyTee2,
+      3: NyTee3,
+      4: NyTee4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 25,
+    name: "Knicks Retro Champions Vintage Tee",
+    price: 646,
+    image: {
+      1: NyChampion1,
+      2: NyChampion2,
+      3: NyChampion3,
+      4: NyChampion4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 26,
+    name: "Knicks Retro Ny Insignia Vintage Tee",
+    price: 646,
+    image: {
+      1: NyInsignia1,
+      2: NyInsignia2,
+      3: NyInsignia3,
+      4: NyInsignia4,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 27,
+    name: "New York Knicks Larry",
+    price: 2766,
+    image: {
+      1: BlueJohnson1,
+      2: BlueJohnson2,
+      3: BlueJohnson3,
+      4: BlueJohnson4,
+      5: BlueJohnson5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 28,
+    name: "Wyona Quarter Zip Sweater",
+    price: 2259,
+    image: {
+      1: BlueWyona1,
+      2: BlueWyona2,
+      3: BlueWyona3,
+      4: BlueWyona4,
+      5: BlueWyona5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 29,
+    name: "Wyona Quarter Zip Sweater",
+    price: 2259,
+    image: {
+      1: GreyWyona1,
+      2: GreyWyona2,
+      3: GreyWyona3,
+      4: GreyWyona4,
+      5: GreyWyona5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 30,
+    name: "Davidson Mock Neck",
+    price: 2259,
+    image: {
+      1: WhiteDavison1,
+      2: WhiteDavison2,
+      3: WhiteDavison3,
+      4: WhiteDavison4,
+      5: WhiteDavison5,
+    },
+    category: `men's top`,
+  },
+  {
+    id: 31,
+    name: "Davidson Mock Neck",
+    price: 2259,
+    image: {
+      1: GreenDavison1,
+      2: GreenDavison2,
+      3: GreenDavison3,
+      4: GreenDavison4,
+      5: GreenDavison5,
+    },
+    category: `men's top`,
+  },
+];
+
+export default mensTop
