00f0b1afd34af0d649b02cbc009977d06a861c2c
Kelvin Achi <kellywizy09@gmail.com>
2024-02-20 15:44:28 +0100

    product page started

---
+++ src/Components/BreadCrum/BreadCrum.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./BreadCrum.css";
+
+const BreadCrum = () => {
+  return (
+    <div className='breadcrum'>
+     
+    </div>
+  )
+}
+
+export default BreadCrum
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,9 +1,34 @@
 import React, { createContext } from "react";
+import mens_accessories from "../Components/Assets/mens_accessories";
+import mens_bottom from "../Components/Assets/mens_bottom";
+import mens_tops from "../Components/Assets/mens_top";
+import mens_outerwear from "../Components/Assets/mens_outerwear";
+import mens_footwear from "../Components/Assets/mens_footwear";
+import womens_accessories from "../Components/Assets/womens_accessories";
+import womens_bottom from "../Components/Assets/womens_bottom";
+import womens_dress from "../Components/Assets/womens_dress";
+import womens_footwear from "../Components/Assets/womens_footwears";
+import womens_top from "../Components/Assets/womens_top";
+import womens_outerwear from "../Components/Assets/womens_outerwear";
 
 export const ShopContext = createContext(null);
 
+const allProducts = [
+  ...mens_accessories,
+  ...mens_bottom,
+  ...mens_tops,
+  ...mens_outerwear,
+  ...mens_footwear,
+  ...womens_accessories,
+  ...womens_bottom,
+  ...womens_dress,
+  ...womens_footwear,
+  ...womens_top,
+  ...womens_outerwear,
+];
+
 const ShopContextProvider = (props) => {
- const contextValue = {}
+ const contextValue = {allProducts}
  return (
   <ShopContext.Provider value={contextValue}>
    {props.children}
