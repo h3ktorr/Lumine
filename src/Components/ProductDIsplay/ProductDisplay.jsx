8449e5286a8d5ee14fbd07ad7fc4c3b75dcc916c
Kelvin Achi <kellywizy09@gmail.com>
2024-03-10 08:27:36 +0100

    cart asset update image from object to array 1

--- src/Components/ProductDIsplay/ProductDisplay.jsx
+++ src/Components/ProductDIsplay/ProductDisplay.jsx
@@ -1,21 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import "./ProductDisplay.css";
 import { ShopContext } from '../../Context/ShopContext';
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 
 const ProductDisplay = (props) => {
- const {product} = props;
+ const {product} = props; 
  const {addToCart} = useContext(ShopContext);
+ const dynamicImage = Object.keys(product.image).map((key) => product.image[key]).map(item => item.replace("/static/media/", ""));
+ const [productImage, setProductImage] = useState(product.image)
  const [index, setIndex] = useState(0);
+ console.log(productImage);
+//  const getImage = () => {
+//   Object.values(product.image).map((item, itemIndex) => {
+//     let position = 'nextSlide';
+//      const dynamicImagePath = item.replace("/static/media/", "");
+//     console.log(dynamicImagePath)
+//     if(itemIndex === index){
+//       position = 'activeSlide';
+//     }
+//     if(itemIndex === index - 1 || (index === 0 && itemIndex === product.image.length - 1)){
+//       position = 'lastSlide';
+//     }
+    
+//   })
+//  }
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
-        <img src={product.image[1]} alt="" />
+        {productImage.map((image, imageIndex) => {
+          return <img src={image} alt="" key={imageIndex} />
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
       </div>
       <div className="productdisplay-right">
         <div className="productdisplay-details">
-          <p className='product-name'>{product.name}</p>
+          <p className="product-name">{product.name}</p>
           <p>${product.price}</p>
           <p>{product.category}</p>
         </div>
@@ -31,7 +56,13 @@ const ProductDisplay = (props) => {
             <div>XXS</div>
           </div>
         </div>
-        <button onClick={()=> {addToCart(product.id)}}>Add to Cart</button>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
